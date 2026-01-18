1722c3a338e3516837cfa8099cce29599fb36c87
kasahara-san <kasahara.yuichiro.res@gmail.com>
2026-01-18 18:46:55 +0900

    Update: ver1(動作検証未実施)の実装完了

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'data'),glob('data/*.yaml')),
         (os.path.join('share', package_name, 'launch'),glob('launch/*.launch.py')),
         (os.path.join('share', package_name, 'json_samples'),glob('json_samples/*.json')),
     ],
@@ -26,7 +27,9 @@ setup(
         'console_scripts': [
             'loader_from_path = tms_if_input.loader_from_path:main',
             'scenario_segmentor = tms_if_input.scenario_segmentor:main',
-            'taskset_compiler = tms_if_input.taskset_compiler:main'
+            'taskset_compiler = tms_if_input.taskset_compiler:main',
+            'db_task_writer = tms_if_input.db_task_writer:main',
+            'db_param_writer = tms_if_input.db_param_writer:main',
         ],
     },
 )
